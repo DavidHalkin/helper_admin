780754b946bd4616fb493773725bafb05c01c53d
David_Halkin <northfire@mail.ru>
2020-05-06 17:06:13 +0300

    sdfsdf

---
+++ app/js/main.js
@@ -0,0 +1,93 @@
+
+// multilevel menu Start
+(function($) {
+	$.fn.vmenuModule = function(option) {
+		var obj,
+			item;
+		var options = $.extend({
+				Speed: 220,
+				autostart: true,
+				autohide: 1
+			},
+			option);
+		obj = $(this);
+
+		item = obj.find("ul").parent("li").children("a");
+		item.attr("data-option", "off");
+
+		item.unbind('click').on("click", function() {
+			var a = $(this);
+			if (options.autohide) {
+				a.parent().parent().find("a[data-option='on']").parent("li").children("ul").slideUp(options.Speed / 1.2,
+					function() {
+						$(this).parent("li").children("a").attr("data-option", "off");
+					})
+			}
+			if (a.attr("data-option") == "off") {
+				a.parent("li").children("ul").slideDown(options.Speed,
+					function() {
+						a.attr("data-option", "on");
+					});
+			}
+			if (a.attr("data-option") == "on") {
+				a.attr("data-option", "off");
+				a.parent("li").children("ul").slideUp(options.Speed)
+			}
+		});
+		if (options.autostart) {
+			obj.find("a").each(function() {
+
+				$(this).parent("li").parent("ul").slideDown(options.Speed,
+					function() {
+						$(this).parent("li").children("a").attr("data-option", "on");
+					})
+			})
+		}
+
+	}
+})(window.jQuery || window.Zepto);
+// multilevel menu END
+
+$.extend( $.fn.dataTable.defaults, {
+    responsive: true
+} );
+$(document).ready(function() {
+	$(".multilevel_menu_js").vmenuModule({
+		Speed: 200,
+		autostart: false,
+		autohide: false
+	});
+	$('.multilevel_menu_js li:has(ul)').addClass("has_drop");
+	$('.multilevel_menu_js li.has_drop > a').on("click",function(){
+		return false;
+	});
+	
+	// search in header
+	$(".form_group_search_js input").focusin(function(){
+		$(this).closest(".form_group_search_js").addClass("opened");
+	});
+	$(".form_group_search_js input").focusout(function(){
+		$(this).closest(".form_group_search_js").removeClass("opened");
+	});
+	// custom select
+	$('.bootstrap_select_js').selectpicker();
+	// dataTables
+	$('.dataTables_js').DataTable({
+		responsive: true
+	});
+	$('.dataTables_minimal_js').DataTable({
+		"responsive": false,
+		// "ordering": false,
+		"bPaginate": false, //hide pagination
+		"bFilter": false, //hide Search bar
+		"bInfo": false, // hide showing entries
+
+	});
+});
+// don't close drop on click
+$(document).on("click.bs.dropdown.data-api", ".noclose", function (e) { e.stopPropagation() });
+
+// dataTables at bootstrap tabs
+$(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function (e) {
+    $.fn.dataTable.tables({ visible: true, api: true }).columns.adjust();
+});
--- gulpfile.js
+++ gulpfile.js
@@ -99,6 +99,8 @@ gulp.task('scripts', function() {
 		'bower_components/jquery/dist/jquery.min.js',
 		// 'bower_components/air-datepicker/dist/js/datepicker.min.js',
 		'bower_components/bootstrap/dist/js/bootstrap.bundle.min.js',
+		'bower_components/bootstrap-select/dist/js/bootstrap-select.min.js',
+		'bower_components/chart.js/dist/Chart.bundle.min.js',
 		// 'node_modules/overlayscrollbars/js/jquery.overlayScrollbars.min.js',
 		])
 		// .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
@@ -111,6 +113,8 @@ gulp.task('scripts', function() {
 gulp.task('css-libs', ['sass'], function() {
 	return gulp.src([ // Берем все необходимые библиотеки
 		'node_modules/@fortawesome/fontawesome-pro/css/all.css',
+		'bower_components/bootstrap-select/dist/css/bootstrap-select.min.css',
+		'bower_components/chart.js/dist/Chart.min.css',
 		// 'bower_components/air-datepicker/dist/css/datepicker.min.css',
 		// 'node_modules/overlayscrollbars/css/OverlayScrollbars.min.css',
 		])
